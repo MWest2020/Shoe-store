8e029109190958e421caf448006ab30501062cb1
MWest2020 <mwesterweel@hotmail.com>
2021-08-11 22:40:02 +0200

    keystone connected

--- shoe-store/backend/keystone.ts
+++ shoe-store/backend/keystone.ts
@@ -1,4 +1,12 @@
 import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+import { ProductImage } from "./schemas/ProductImage";
+import { Product } from "./schemas/Product";
+import { User } from "./schemas/User";
 import "dotenv/config";
 
 const databaseURL =
@@ -9,25 +17,45 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  // @ts-ignore
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
   },
-  db: {
-    adapter: "mongoose",
-    url: databaseURL,
-    // TODO: Add data seeding here
-  },
-  lists: createSchema({
-    // Schema items go in here
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session values here
 });
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      // TODO: Add data seeding here
+      onConnect() {
+        console.log("connected to the database");
+      },
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // TODO: change this for roles
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    // TODO: Add session values here
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: "id name email",
+    }),
+  })
+);
